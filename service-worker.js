0b1291844e45558f774e817733fca061f242e50f
charitharumapperuma <charith.11@cse.mrt.ac.lk>
2021-10-05 18:19:04 +0000

    Deploying to gh-pages from  @ 5d238ed63fb660e1c0e438c08debd566275dad48 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c185cef1beb736f05be62f40e943b66d.js"
+  "www.execomtech.com/precache-manifest.09069425d838e08fda999d1368d021e7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("www.execomtech.com/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
